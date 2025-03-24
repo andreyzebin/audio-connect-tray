0bc252c03f941da69c69ca85a93e109ed1500788
Andrey Zabebenin <andrey.zebin@gmail.com>
2025-03-24 10:47:52 +0300

    Added java checks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,15 +7,17 @@
 
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
-    // id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
+// use only for home/dev use - e.q.: no enterprise repositories configuration
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenLocal()
         mavenCentral()
         maven( "https://jitpack.io" )
+        gradlePluginPortal()
     }
 }
 
